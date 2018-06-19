46b4754804be6242007002f95321735c0f98c8be
Nandita <carlos@dynetv.com>
2018-06-18 20:47:59 -0400

    v05

--- develop/Find and replace Ass File.py
+++ develop/Find and replace Ass File.py
@@ -3,37 +3,6 @@ from c4d import gui
 
 ARNOLD_PROCEDURAL = 1032509
 
-class OptionsDialog(gui.GeDialog):
-
-    IDC_LABELNAME = 1000
-    IDC_EDITNAME = 1001
-
-    def CreateLayout(self):
-
-        self.SetTitle('Crypto User AOV')
-
-        self.AddStaticText(self.IDC_LABELNAME, c4d.BFH_LEFT, name='Set User Data Name:') 
-        self.AddEditText(self.IDC_EDITNAME, c4d.BFH_SCALEFIT)
-        self.SetString(self.IDC_EDITNAME, 'Write Crypto User Data AOV')
-
-        # Ok/Cancel buttons
-        self.AddDlgGroup(c4d.DLG_OK|c4d.DLG_CANCEL)
-        self.ok = False
-        return True
-
-    def Command(self, id, msg):
-
-        if id == c4d.IDC_OK:
-            self.ok = True
-            self.findGName = self.GetString(self.IDC_EDITNAME)
-            self.Close()
-
-        elif id == c4d.IDC_CANCEL:
-            self.Close()
-            gui.MessageDialog('Please select a user Cryptomatte input user data.')
-
-        return True
-
 def get_active_objs(): # get active objects from obj manager
     activeObjects = doc.GetActiveObjects(c4d.GETACTIVEOBJECTFLAGS_CHILDREN)
     if not activeObjects:
@@ -46,14 +15,6 @@ def main():
     if objsList == None:
         return
 
-    # Open the string dialog
-    """dlg = OptionsDialog()
-    dlg.Open(c4d.DLG_TYPE_MODAL, defaultw=300, defaulth=50)
-    if not dlg.ok:
-        return
-
-    procedural_folder = dlg.findGName"""
-
     mayaProcedural_name = 'ArnoldStandIn'
     procedurals_list = []
 
@@ -64,10 +25,6 @@ def main():
             procedural = c4d.BaseObject(ARNOLD_PROCEDURAL)
             procedural.SetName(procedural_name)
 
-            # scale unit parameters
-            #procedural.GetOpContainerInstance().SetFloat((c4d.C4DAI_PROCEDURAL_SCALE, 0.1)
-            #procedural[c4d.C4DAI_PROCEDURAL_SCALE] = 0.1
-            #UnitScaleData.SetUnitScale(scale, unit)
             """procedural_scale = procedural[c4d.C4DAI_PROCEDURAL_SCALE]
             print 'checkpoint 01'
             print procedural_scale.GetUnitScale()
@@ -79,6 +36,8 @@ def main():
             procedural[c4d.ID_BASEOBJECT_REL_SCALE] = c4d.Vector(0.01,0.01,0.01)
             procedural[c4d.C4DAI_PROCEDURAL_OBJECT_DISPLAY_MODE] = 2 # point cloud mode
             path = c4d.storage.LoadDialog(title = 'select: ' + procedural_name)
+            if not path:
+                return
             procedural[c4d.C4DAI_PROCEDURAL_PATH] = path
             doc.InsertObject(procedural)
 
@@ -93,13 +52,3 @@ def main():
 
 if __name__=='__main__':
     main()
-
-
-'D:\01_Documents\Dyne\Dyne - Works\Quality Metal - Product Shots\Maya\qm_modeling\assets\ass files\level.ass.gz'
-
-'D:\01_Documents\Dyne\Dyne - Works\Quality Metal - Product Shots\Maya\qm_modeling\assets\ass files\level_ass.ass.gz'
-
-'D:\01_Documents\Dyne\Dyne - Works\Quality Metal - Product Shots\Maya\qm_modeling\assets\ass files\level_ass.gz'
-
-'D:\01_Documents\Dyne\Dyne - Works\Quality Metal - Product Shots\Maya\qm_modeling\assets\ass files\level.ass.gz'
-
